7d3fe3f486cc22b4bb64c8dcabaf179c0ae479df
Mohammad Swlha <mohammad80111@gmail.com>
2025-08-01 17:46:21 +0300

    Added responsive image with media queries

--- MyPoriflo/Program.cs
+++ MyPoriflo/Program.cs
@@ -1,46 +1,4 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using MyPoriflo.Models;
-//using System;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-
-////
-////
-//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
-//    builder.Configuration.GetConnectionString("MyConn")
-//    ));
-
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.Run();
-
-
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MyPoriflo.Models;
 using System;
 
@@ -49,9 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+//
+//
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("MyConn")
-));
+    ));
+
 
 var app = builder.Build();
 
@@ -59,6 +21,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -73,10 +36,52 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// **هذا الجزء نضيفه هنا**
-var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
-app.Urls.Clear();
-app.Urls.Add($"http://*:{port}");
-
-// ثم تشغيل التطبيق
 app.Run();
+
+
+
+
+
+
+
+
+//using Microsoft.EntityFrameworkCore;
+//using MyPoriflo.Models;
+//using System;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+//    builder.Configuration.GetConnectionString("MyConn")
+//));
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//// **هذا الجزء نضيفه هنا**
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+//app.Urls.Clear();
+//app.Urls.Add($"http://*:{port}");
+
+//// ثم تشغيل التطبيق
+//app.Run();
